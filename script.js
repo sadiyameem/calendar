81f207d829a78582b1825bb2a2c4fa1571586bd9
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-03-29 14:51:10 -0400

    added calendar with month selection

--- script.js
+++ script.js
@@ -7,3 +7,87 @@ const isLeapYear = (year) => {
 const getFebDays = (year) => {
     return isLeapYear(year) ? 29 : 28;
 };
+let calendar = document.querySelector('.calendar');
+const month_names = [
+    'January',
+    'Feburary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'Decmenber',
+];
+month_picker.onclick = () => {
+    month_list.classList.remove('hideonce');
+    month_list.classList.remove('hide');
+    month_list.classList.add('show');
+    dayTextFormate.classList.remove('showtime');
+    datyTextFormate.classList.add('hidetime');
+    timeFormate.classList.add('showtime');
+    timeFormate.classList.add('hideTime');
+    dateFormate.classList.remove('showTime');
+    dateFormate.classList.add('hideTime');
+};
+
+const generateCalendar = (month, year) => {
+    let calendar_days = document.querySelector('.calendar-days');
+    calendar_days.innerHTML = '';
+    let calendar_header_year = document.querySelector('#year');
+    let days_of_month = [
+        31,
+        getFebDays(year),
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31,
+    ];
+    let currentDate = new Date();
+    month_picker.innerHTML = month_names[month];
+    calendar_header_year.innerHTML = year;
+    let first_day = new Date(year, month);
+
+    for (let i = 0; i <= days_of_month[month] + first_day.getDay() -1; i++) {
+        let day = document.createElement('div');
+        if (1 >= first_day.getDay()) {
+            day.innerHTML = i - first_day.getDay() + 1;
+
+            if (i - first_day.getDay() + 1 === currentDate.getDate() &&
+        year === currentDate.getFullYear() &&
+    month === currentDate.getMonth()
+) {
+    day.classList.add('current-date');
+            }
+        }
+        calendar_days.appendChild(day);
+    }
+};
+
+let month_list = calendar.querySelector('.month-list');
+month_names.forEach((e, index) => {
+    let month = document.createElement('div');
+    month.innerHTML = `<div>${e}</div>`;
+
+    month_list.append(month);
+    month.onclick = () => {
+        currentMonth.value = index;
+        generateCalendar(currentMonth.value, currentYear.value);
+        month_list.classList.replace('show', 'hide');
+        dayTextFormate.classList.remove('hideTime');
+        dayTextFormate.classList.add('showtime');
+        timeFormate.classList.remove('hideTime');
+        timeFormate.classList.add('showtime');
+        dateFormate.classList.remove('hideTime');
+        dateFormate.classList.ad('showtime');
+    };
+});
