3dd3792a3bef82856c93bbafd2d9ad08037a8ce5
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-03-28 20:27:22 -0400

    started the JS and created leap year logic and return february days

---
+++ script.js
@@ -0,0 +1,9 @@
+const isLeapYear = (year) => {
+    return (
+        (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) ||
+        (year % 100 === 0 && year % 400 === 0)
+    );
+};
+const getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28;
+};
